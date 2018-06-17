353e3e846e9732da4125ba65b351dd98f34c02dc
Roman Hellmüller <roman.hellmueller@gmail.com>
2018-06-17 17:02:47 +0200

    updated data model

--- sql/createcourse.sql
+++ sql/createcourse.sql
@@ -2,7 +2,8 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
--- TODO check if db exists - if yes, make a dump/rename and delete it
+-- create a new course - a random scheme needs to be picked and tables created
+
 DROP TABLE IF EXISTS `leader`;
 DROP TABLE IF EXISTS `course`;
 
@@ -27,5 +28,66 @@ CREATE TABLE IF NOT EXISTS `course` (
 `courseId` int(11) NOT NULL,
   `courseName` int(11) NOT NULL,
   `dbScheme` varchar(255) COLLATE utf8_bin NOT NULL
+  -- start date
+  -- end date
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+CREATE TABLE IF NOT EXISTS `participant` (
+`participantId` int(11) NOT NULL,
+  `participantName` varchar(255) COLLATE utf8_bin NOT NULL,
+    `participantSurname` varchar(255) COLLATE utf8_bin NOT NULL,
+    `participantScoutname` varchar(255) COLLATE utf8_bin NOT NULL
+-- age
+-- Unit
+-- Group
+-- information from Recommender
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+CREATE TABLE IF NOT EXISTS `participantTag` (
+`participantTagId` int(11) NOT NULL,
+`parentParticipantTagId` int(11) NULL,
+  `participantTagName` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+CREATE TABLE IF NOT EXISTS `observationTag` (
+`observationTagId` int(11) NOT NULL,
+`parentObservationTagId` int(11) NULL,
+  `observationTagName` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
+CREATE TABLE IF NOT EXISTS `observation` (
+`observationId` int(11) NOT NULL,
+  `observationText` varchar(4096) COLLATE utf8_bin NOT NULL,
+  `activityId` int(11)  NULL,
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
+CREATE TABLE IF NOT EXISTS `activity` (
+`activityId` int(11) NOT NULL,
+  `activityName` varchar(255) COLLATE utf8_bin NOT NULL,
+  `activityNumber` varchar(255) COLLATE utf8_bin NOT NULL,
+  `activityDate` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
+CREATE TABLE IF NOT EXISTS `codeMapping` (
+`participantTagId` int(11) NOT NULL,
+  `courseName` int(11) NOT NULL,
+  `dbScheme` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
+
+
+
+
+
 
