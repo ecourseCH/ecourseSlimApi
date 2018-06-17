22bf5021455c83b4fe6b4a6026bd7d234d55c12e
Roman Hellmüller <roman.hellmueller@gmail.com>
2018-06-17 17:04:55 +0200

    updated data base model

--- sql/buildecourse.sql
+++ sql/buildecourse.sql
@@ -1,22 +1,29 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+-- TODO make sequencer
 
--- TODO check if db exists - if yes, make a dump/rename and delete it
-DROP TABLE IF EXISTS `user`;
-DROP TABLE IF EXISTS `course`;
+-- TODO check if db exists - if yes, make a dump/rename and delete it 
+DROP DATABASE IF EXISTS ecourse;
+
+CREATE DATABASE ecourse;
+
+USE ecourse;
+
+-- No need to drop tables as db was droped
+-- DROP TABLE IF EXISTS `course`;
+-- DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE IF NOT EXISTS `user` (
 `userId` int(11) NOT NULL,
   `userName` varchar(255) COLLATE utf8_bin NOT NULL,
   `userMail` varchar(255) COLLATE utf8_bin NOT NULL,
   `password` varchar(255) COLLATE utf8_bin NOT NULL,
-      `language` varchar(8) COLLATE utf8_bin NOT NULL
+      `language` varchar(8) COLLATE utf8_bin NOT NULL,
+        PRIMARY KEY (`userId`),
+  UNIQUE KEY `userMail` (`userMail`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`userId`);
 
 INSERT INTO `user` (`userId`, `userName`, `userMail`, `password`, `language`) values
 (0, 'admin', 'admin@localhost', 'root', 'CH_de');
@@ -25,8 +32,12 @@ INSERT INTO `user` (`userId`, `userName`, `userMail`, `password`, `language`) va
 CREATE TABLE IF NOT EXISTS `course` (
 `courseId` int(11) NOT NULL,
   `courseName` int(11) NOT NULL,
-  `dbScheme` varchar(255) COLLATE utf8_bin NOT NULL
+  `ownerUserId` int(11) NOT NULL,
+  `dbScheme` varchar(255) COLLATE utf8_bin NOT NULL,
+    -- start date
+  -- end date
+          PRIMARY KEY (`courseId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
-
+ALTER TABLE course ADD CONSTRAINT fk_user_id FOREIGN KEY (ownerUserId) REFERENCES user(userId);
--- sql/createcourse.sql
+++ sql/createcourse.sql
@@ -2,61 +2,68 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
--- create a new course - a random scheme needs to be picked and tables created
+-- TODO create a new course - a random dbname needs to be picked, created and used
+-- tables to be created in new db
 
+
+
+DROP TABLE IF EXISTS `participant`;
+DROP TABLE IF EXISTS `participantTag`;
+DROP TABLE IF EXISTS `observationTag`;
+DROP TABLE IF EXISTS `observation`;
+DROP TABLE IF EXISTS `activity`;
+DROP TABLE IF EXISTS `codeMapping`;
 DROP TABLE IF EXISTS `leader`;
-DROP TABLE IF EXISTS `course`;
+
 
 CREATE TABLE IF NOT EXISTS `leader` (
 `leaderId` int(11) NOT NULL,
 `userId` int(11) NOT NULL,
   `leaderName` varchar(255) COLLATE utf8_bin NOT NULL,
     `leaderSurname` varchar(255) COLLATE utf8_bin NOT NULL,
-    `leaderScoutname` varchar(255) COLLATE utf8_bin NOT NULL
+    `leaderScoutname` varchar(255) COLLATE utf8_bin NOT NULL,
+     PRIMARY KEY (`leaderId`),
+     UNIQUE KEY (`userId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
-ALTER TABLE `leader`
- ADD PRIMARY KEY (`leaderId`);
- 
- ALTER TABLE `leader`
- ADD UNIQUE KEY (`userId`);
+-- TODO might not work accross db's
+ALTER TABLE leader ADD CONSTRAINT fk2_user_id FOREIGN KEY (userId) REFERENCES user(userId);
 
-
-
-CREATE TABLE IF NOT EXISTS `course` (
-`courseId` int(11) NOT NULL,
-  `courseName` int(11) NOT NULL,
-  `dbScheme` varchar(255) COLLATE utf8_bin NOT NULL
-  -- start date
-  -- end date
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
 CREATE TABLE IF NOT EXISTS `participant` (
 `participantId` int(11) NOT NULL,
   `participantName` varchar(255) COLLATE utf8_bin NOT NULL,
     `participantSurname` varchar(255) COLLATE utf8_bin NOT NULL,
-    `participantScoutname` varchar(255) COLLATE utf8_bin NOT NULL
+    `participantScoutname` varchar(255) COLLATE utf8_bin NOT NULL,
 -- age
 -- Unit
 -- Group
 -- information from Recommender
+     PRIMARY KEY (`participantId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-
+-- TODO maybe needs to be linked to observation /activity for purpose of observation requests
 CREATE TABLE IF NOT EXISTS `participantTag` (
 `participantTagId` int(11) NOT NULL,
 `parentParticipantTagId` int(11) NULL,
-  `participantTagName` varchar(255) COLLATE utf8_bin NOT NULL
+  `participantTagName` varchar(255) COLLATE utf8_bin NOT NULL,
+       PRIMARY KEY (`participantTagId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
-CREATE TABLE IF NOT EXISTS `observationTag` (
-`observationTagId` int(11) NOT NULL,
-`parentObservationTagId` int(11) NULL,
-  `observationTagName` int(11) NOT NULL
+-- TODO intersection leader - participantTag
+
+-- TODO intersection participant Tag - participant
+
+CREATE TABLE IF NOT EXISTS `activity` (
+`activityId` int(11) NOT NULL,
+  `activityName` varchar(255) COLLATE utf8_bin NOT NULL,
+  `activityNumber` varchar(255) COLLATE utf8_bin NOT NULL,
+  `activityDate` DATETIME NOT NULL,
+       PRIMARY KEY (`activityId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 
 
 
@@ -64,23 +71,41 @@ CREATE TABLE IF NOT EXISTS `observation` (
 `observationId` int(11) NOT NULL,
   `observationText` varchar(4096) COLLATE utf8_bin NOT NULL,
   `activityId` int(11)  NULL,
+  `observationDate` DATETIME NULL,
+  `leaderId` int(11) NOT NULL,
+  `participantId` int(11) NOT NULL,
+       PRIMARY KEY (`observationId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+ALTER TABLE observation ADD CONSTRAINT fk_activity_id FOREIGN KEY (activityId) REFERENCES activity(activityId);
+
+-- TODO check might be 1..n - 0..n
+ALTER TABLE observation ADD CONSTRAINT fk_leader_id FOREIGN KEY (leaderId) REFERENCES leader(leaderId);
+
+-- TODO check might be 1..n - 0..n
+ALTER TABLE observation ADD CONSTRAINT fk_participant_id FOREIGN KEY (participantId) REFERENCES participant(participantId);
 
 
-CREATE TABLE IF NOT EXISTS `activity` (
-`activityId` int(11) NOT NULL,
-  `activityName` varchar(255) COLLATE utf8_bin NOT NULL,
-  `activityNumber` varchar(255) COLLATE utf8_bin NOT NULL,
-  `activityDate` varchar(255) COLLATE utf8_bin NOT NULL
+CREATE TABLE IF NOT EXISTS `observationTag` (
+`observationTagId` int(11) NOT NULL,
+`parentObservationTagId` int(11) NULL,
+  `observationTagName` int(11) NOT NULL,
+       PRIMARY KEY (`observationTagId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- TODO intersection observation - observation Tag
+
 
 
 
 CREATE TABLE IF NOT EXISTS `codeMapping` (
-`participantTagId` int(11) NOT NULL,
-  `courseName` int(11) NOT NULL,
-  `dbScheme` varchar(255) COLLATE utf8_bin NOT NULL
+`codeMappingId` int(11) NOT NULL,
+  `codeMappingName` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Key1` int(11)  NULL,
+  `Value1` varchar(255) COLLATE utf8_bin  NULL,
+   PRIMARY KEY (`codeMappingId`),
+     UNIQUE KEY (`codeMappingName`)
+
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
