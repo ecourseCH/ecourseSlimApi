29c352a370020fa24ec57c1bfa55f0002530cec9
Roman Hellmüller <roman.hellmueller@gmail.com>
2018-08-01 11:12:19 +0200

    enhance installer script with composer

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ if [ -z "$1" ]
     echo "eCourse db sucessfully setup"
   else
     echo "problem with eCourse db setup."
+    exit 1
   fi
 else
 #TODO considered insecure
@@ -39,22 +40,73 @@ mysql -u root -p$1 < ./sql/buildecourse.sql
     echo "eCourse db sucessfully setup"
   else
     echo "problem with eCourse db setup."
+    exit 1;
   fi
 
 fi
 
-if [ -d "/var/www/ecourse" ]; then
-  echo "eCourse directory already exists. Please delete first"
+if [ -d $DIR ]; then
+  echo "eCourse directory already exists. Please delete first";
+  
+   read -p 'Would you like to delete it (y/n)? ' DELDIR;
+  if [ ${DELDIR,,} == 'y' ] ; then
+    rm -rdf $DIR;
+   if [ "$?" -eq 0 ]
+   then
+    echo "eCourse directory deletion successful"
+  else
+    echo "problem with eCourse directory deletion"
+    exit 1;
+  fi
+   
+   
+  else
+  echo " Please remove directory manually and restart installation"
+  exit 1;
+  fi
  else
- mkdir /var/www/ecourse
+ mkdir $DIR
    if [ "$?" -eq 0 ]
    then
     echo "eCourse directory sucessfully setup"
   else
     echo "problem with eCourse directory creation."
+    exit 1;
   fi
 
 fi
 
 rsync -avz --exclude 'install.sh' --exclude 'sql/buildecourse.sql' --exclude '.git' . $DIR
+ if [ "$?" -eq 0 ]
+   then
+    echo "eCourse copied to webserver location sucessfully setup"
+  else
+    echo "problem with copying eCourse to webserver location."
+    exit 1;
+  fi
 
+# install composer
+cd $DIR;
+
+./install_composer.sh
+ if [ "$?" -eq 0 ]
+   then
+    echo "download composer successful"
+  else
+    echo "download composer failed."
+    exit 1;
+  fi
+
+ php composer.phar install
+  if [ "$?" -eq 0 ]
+   then
+    echo "composer sucessfully setup"
+  else
+    echo "problem with composer setup"
+    exit 1;
+  fi
+
+#echo "composer installed, including depenencies"
+
+# start webservice
+# php -S localhost:3000
