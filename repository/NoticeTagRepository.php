da34709fa561277fa885800babe3b465c23a6170
Carlo Beltrame <carlo@beltra.me>
2018-11-07 15:21:36 +0100

    Use generic read method for noticeTags

--- repository/NoticeTagRepository.php
+++ repository/NoticeTagRepository.php
@@ -10,28 +10,6 @@ class NoticeTagRepository extends Repository {
         return ['noticeTagId', 'noticeTagName', 'parentNoticeTagId'];
     }
 
-    public function getNoticeTags() {
-        $sql = "SELECT nt.noticeTagId, nt.noticeTagName, nt.parentNoticeTagId
-            FROM noticeTag nt";
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute();
-        $results = [];
-        while ($row = $stmt->fetch()) {
-            $rows[] = $row;
-            $categories[$row['parentNoticeTagId']][] = $row;
-        }
-
-
-        $result = array();
-        foreach ($rows as &$resultRow) {
-            if ($resultRow['parentNoticeTagId'] == null || $resultRow['parentNoticeTagId'] == 0) {
-                $result[] = $this->getChildrenOfNoticeTag($resultRow, $categories);
-            }
-        }
-
-        return $result;
-    }
-
     private function getChildrenOfNoticeTag($noticeTag, &$categories) {
         if (!array_key_exists($noticeTag['noticeTagId'], $categories)) {
             return $noticeTag;
