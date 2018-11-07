836c69bffed260451138dc442400ff3068d48620
Carlo Beltrame <carlo@beltra.me>
2018-11-07 15:41:42 +0100

    Better naming for repository helper methods

--- repository/Repository.php
+++ repository/Repository.php
@@ -21,19 +21,19 @@ abstract class Repository {
         return '`' . $fieldName . '`';
     }
 
-    private static function wrapFieldValueWithColon($fieldValue) {
+    private static function prefixFieldValueWithColon($fieldValue) {
         return ':' . $fieldValue;
     }
 
-    private static function wrapFieldNameForUpdate($fieldName) {
-        return self::wrapFieldNameInQuotes($fieldName) . ' = ' . self::wrapFieldValueWithColon($fieldName);
+    private static function createEqualsExpression($fieldName) {
+        return self::wrapFieldNameInQuotes($fieldName) . ' = ' . self::prefixFieldValueWithColon($fieldName);
     }
 
     public function add($entity) {
         $entityName = $this->getEntityName();
         $entityFields = $this->getEntityFields();
         $entityFieldsString = implode(",", array_map(['self', 'wrapFieldNameInQuotes'], $entityFields));
-        $entityFieldValuesString = implode(", ", array_map(['self', 'wrapFieldValueWithColon'], $entityFields));
+        $entityFieldValuesString = implode(", ", array_map(['self', 'prefixFieldValueWithColon'], $entityFields));
 
         $sql = "INSERT INTO $entityName ( $entityFieldsString ) VALUES ($entityFieldValuesString)";
         $stmt = $this->db->prepare($sql);
@@ -66,7 +66,7 @@ abstract class Repository {
         $entityName = $this->getEntityName();
         $publicFieldsString = implode(', ', array_map(['self', 'wrapFieldNameInQuotes'], $this->getPublicFields()));
         $idFieldName = $this->getIdName();
-        $idFieldCondition = self::wrapFieldNameForUpdate($idFieldName);
+        $idFieldCondition = self::createEqualsExpression($idFieldName);
 
         $sql = "SELECT $publicFieldsString FROM $entityName WHERE $idFieldCondition";
         $stmt = $this->db->prepare($sql);
@@ -80,9 +80,9 @@ abstract class Repository {
     public function update($id, $entity) {
         $entityName = $this->getEntityName();
         $entityFields = $this->getEntityFields();
-        $entityFieldAssignments = implode( ', ', array_map(['self', 'wrapFieldNameForUpdate'], $entityFields));
+        $entityFieldAssignments = implode( ', ', array_map(['self', 'createEqualsExpression'], $entityFields));
         $idFieldName = $this->getIdName();
-        $idFieldCondition = self::wrapFieldNameForUpdate($idFieldName);
+        $idFieldCondition = self::createEqualsExpression($idFieldName);
         // Prevent updating id
         unset($entity[$idFieldName]);
 
@@ -109,7 +109,7 @@ abstract class Repository {
     public function deleteById($id) {
         $entityName = $this->getEntityName();
         $idFieldName = $this->getIdName();
-        $idFieldCondition = self::wrapFieldNameForUpdate($idFieldName);
+        $idFieldCondition = self::createEqualsExpression($idFieldName);
 
         $sql = "DELETE FROM $entityName WHERE $idFieldCondition";
         $stmt = $this->db->prepare($sql);
